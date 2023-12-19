ac771e57117ed325b63903be0b984b8715237810
Anas Ahmed <anas31197@gmail.com>
2023-12-19 22:03:17 +0530

    Price section (#2)
    * price section first push
    
    * implemented responsive design
    
    ---------
    
    Co-authored-by: ajsinp <ajaysingh98147@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import Pricing from "./components/pricing"
 import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <HeroSection />
-  );
-}
+    <Pricing/>
+  )
+};
 
 export default App;
