192ef68725e9eb11750625a075b61e9b9626da9a
Nawaz Sharief <nsharief330@gmail.com>
2023-12-19 22:41:07 +0530

    footerfinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from "react"
+import Footer from "./components/Footer"
+import Sample from "./components/Sample"
+
 function App() {
 
   return (
-    <div className="w-screen h-screen bg-[#000]">
-      Fellow
-    </div>
+    // <div className="w-screen h-screen bg-[#000]">
+    //   Fellow
+    // </div>
+    <>
+      <Footer/>
+      {/* <Sample/> */}
+    </>
   )
 }
 
