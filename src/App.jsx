b4174c2487eca1a97972fc7152549ba08ad17c97
codeWodeWithArchi <archiwalia948@gmail.com>
2023-12-20 23:07:11 +0530

    final output

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import Footer from "./components/Footer";
 import Learn from "./components/Learn/Learn";
 import Pricing from "./components/pricing";
+import EngagingLearning from "./components/EngagingLearning/EngagingLearning";
 import HeroSection from "./components/HeroSection";
 import LoveFellow from "./components/lovefellow/loveFellow";
 
 function App() {
   return (
     <>
-      <HeroSection />
-      <LoveFellow />
-      <Learn />
+      {/* <HeroSection /> */}
+      <div className="w-full  flex justify-center items-center bg-white">
+      <EngagingLearning/>
+      </div>
+      {/* <LoveFellow />
       <Pricing />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
